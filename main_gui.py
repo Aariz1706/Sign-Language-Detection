0acee66db753270878446a9e7d293433a47a40ae
Aariz Bhamani <aariz1706@gmail.com>
2025-05-24 22:20:39 -0400

    Update main_gui.py

--- main_gui.py
+++ main_gui.py
@@ -11,6 +11,8 @@ window.geometry("400x200")
 
 label = tk.Label(window , text = "Prediction: " , font = ("Arial" , 20)) #Label widget or button to show prediction
 label.pack(pady = 20) #Adds label with paddin g tot he window
+word_label = tk.Label(window , text = "Word:" , font = ("Arial" , 24))
+word_label.pack(pady = 20)
 
 try:
     model = joblib.load("sign_knn_model.pkl") #Load the pre-trained KNN model
@@ -22,7 +24,11 @@ mp_hands = mp.solutions.hands #Access mediapipe hands soln
 mp_drawing = mp.solutions.drawing_utils #Utilities to draw landmarks
 hands = mp_hands.Hands(min_detection_confidence = 0.7 , min_tracking_confidence = 0.5) #Initialize hand detection with CI
 
+buffer_letter = []
+word = ""
+
 def video_loop():
+    global word
     cap = cv2.VideoCapture(0)
     while True:
         success , frame = cap.read() #Capture a single frame
@@ -34,6 +40,7 @@ def video_loop():
         frame = cv2.resize(frame , (480,480))
         rgb_frame = cv2.cvtColor(frame , cv2.COLOR_BGR2RGB) #Converting to rgb in mediapipe
         results = hands.process(rgb_frame)
+        predicted_letter = ""
         if results.multi_hand_landmarks: #If mp detected any hands
             for hand_landmarks in results.multi_hand_landmarks: #Loop from each hand that is 21 landmarks of each hand
                 mp_drawing.draw_landmarks(frame , hand_landmarks , mp_hands.HAND_CONNECTIONS) #frame- video image, hand_landmarks- data for that hand, and mp_hands.HAND_CONNECTIONS- which landmarks are connected by lines.
@@ -41,11 +48,18 @@ def video_loop():
                 for lm in hand_landmarks.landmark:
                     landmarks.extend([lm.x , lm.y , lm.z])
                 landmarks_np = np.array(landmarks).reshape(1 , -1)
-                try:
-                    predicted_label = model.predict(landmarks_np)[0]
-                    label.config(text = f"Prediction: {predicted_label}")
-                except Exception as e:
-                    print(f"Prediction Error:{e}")
+                predicted_letter = model.predict(landmarks_np)[0]
+
+            buffer_letter.append(predicted_letter)
+            if len(buffer_letter) > 15:
+                buffer_letter.pop(0)
+            most_common_letter = max(set(buffer_letter) , key = buffer_letter.count)
+            frequency = buffer_letter.count(most_common_letter)
+            if frequency > 12 and (len(word) == 0 or most_common_letter != word[-1]):
+                word +=most_common_letter
+                buffer_letter.clear()
+                word_label.config(text = f"Word: {word}")
+            label.config(text = f"Prediction: {predicted_letter}")
         cv2.imshow("Hand Tracker" , frame)
         if cv2.waitKey(1) & 0xff == ord("q"):
             break
